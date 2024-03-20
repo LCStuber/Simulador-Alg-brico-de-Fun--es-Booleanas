8d13dc344fad85fff9dd7f70452d0b7282570205
MateusCMartins724 <mateus.c.martins@outlook.com>
2024-03-20 15:29:45 -0300

    adicao da das checagens e input do usuario

--- Simulador.py
+++ Simulador.py
@@ -20,33 +20,51 @@ def formacao_codigos(qntd):
         lista_codigos.append(codigo_binario)
     return lista_codigos        
 
-def formatacao_expressao0(expressao):
-    expressao = expressao.replace("a", "True")
-    expressao = expressao.replace("b", "False")
-    expressao = expressao.replace("c", "True")
-    print(expressao)
-    return expressao
-
-def formatacao_expressao(expressao):
-    expressao = expressao.replace("~", "not")
-    expressao = expressao.replace("+", "or")
-    expressao = expressao.replace("*", "and")
-
-    return expressao
-
-def eval_expressao(expressao, expressao_codigos, letras):
-    for codigos in expressao_codigos:
+def montagem_expressoes(expressao, lista_codigos, letras):
+    lista_expressoes = []
+    for codigos in lista_codigos:
+        expressao_temp = expressao  
         for i,l in enumerate(letras):
-            expressao.replace(codigos[i], l)
+            expressao_temp = expressao_temp.replace(l, str(codigos[i]))
+        expressao_temp = expressao_temp.replace("0", "False")
+        expressao_temp = expressao_temp.replace("1", "True")
+        lista_expressoes.append(expressao_temp)
+    return lista_expressoes
+
+
+def formatacao_expressao(lista_expressoes):
+    i = 0
+    while i < len(lista_expressoes):
+        lista_expressoes[i] = lista_expressoes[i].replace("~", "not")
+        lista_expressoes[i] = lista_expressoes[i].replace("+", "or")
+        lista_expressoes[i] = lista_expressoes[i].replace("*", "and")
+
+        i += 1
+
+    return lista_expressoes
+
+def montagem_tabela(lista_expressoes):
+    for i, expressao in enumerate(lista_expressoes):
+        print("Expressão:", i+1)
         print(expressao)
         print(eval(expressao))
+        print("\n")
 
-expressao = "a*(b*c)+a*~b*~(~a*~c)"
-expressao = expressao_espaco(expressao)
 
-qntd_letras, letras = quantidade_letras(expressao)
-expressao_codigos = formacao_codigos(qntd_letras)
+if __name__ == "__main__":
+    print("-"*30)
+    print("Simulador de Expressões Lógicas")
+    print("-"*30)
+    print("Digite a expressão lógica:")
 
-expressao = formatacao_expressao0(expressao)
-expressao = formatacao_expressao(expressao)
-print(eval_expressao(expressao, expressao_codigos, letras))
+    expressao = str(input())
+
+    expressao = expressao_espaco(expressao)
+
+    qntd_letras, letras = quantidade_letras(expressao)
+    lista_codigos = formacao_codigos(qntd_letras)
+
+    lista_expressoes = montagem_expressoes(expressao, lista_codigos, letras)
+    lista_expressoes = formatacao_expressao(lista_expressoes)
+
+    montagem_tabela(lista_expressoes)
